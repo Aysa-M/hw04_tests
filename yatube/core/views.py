4b2d4d4f0046dd7371eae9a242e61ed128f6eb14
Aysa Matsakova <happiness390@gmail.com>
2022-01-18 22:15:19 +0300

    Исправление кода в соответствие с PEP8.

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from http import HTTPStatus
 
+
 def page_not_found(request, exception):
     """
     Отображение страницы ошибки 404 NOT Found.
@@ -9,6 +10,7 @@ def page_not_found(request, exception):
                   'core/404.html',
                   {'path': request.path},
                   status=HTTPStatus.NOT_FOUND)
+
 
 def csrf_failure(request, reason=''):
     """
